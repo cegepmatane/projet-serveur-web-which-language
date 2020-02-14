36ced65d3dbef6f498c32fc2e1b82d207c9f62a8
emustle <hyvong.georges@gmail.com>
2020-02-14 12:15:14 -0500

    Création du répertoire administration
    Avec tous les fichiers nécessaire aux fonctions d'ajout, de modif et de
    suppression

---
+++ administration/detail-langage.php
@@ -0,0 +1,48 @@
+<?php
+
+    include("connexion.php");
+
+    $id = $_GET["id"];
+
+    $set_utf=$connexion->prepare("SET NAMES UTF8"); 
+    $set_utf->execute(); 
+
+    $REQUETE_DETAIL_LANGAGE = "SELECT id, nom, auteur, date, description, utilisation FROM langage WHERE id=" . $id;
+
+    $requete = $connexion->prepare($REQUETE_DETAIL_LANGAGE);
+    $requete->execute();
+    $langage = $requete->fetch();
+?>
+
+<html lang="fr" xml:lang="fr">
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+        <title>Le langage <?= $langage["nom"] ?></title>
+        <link rel="stylesheet" type="text/css" href="css/liste.css">
+    </head>
+    <body>
+        <div id="contenu-page">
+            <header>
+                <h1>Détail du langage <?= $langage["nom"] ?></h1>
+            </header>
+            <div id="liste-item">
+                <div class="item-box">
+                    <div class="item-text">
+                        <h3 class="item-name"><?= $langage["nom"] ?></h3>
+                        <h4>Auteur(s)</h4>
+                        <p class="item-author"><?= $langage["auteur"] ?></p>
+                        <h4>Première version</h4>
+                        <p class="item-date"><?= $langage["date"] ?></p>
+                        <h4>Description</h4>
+                        <p class="item-desc"><?= $langage["description"] ?></p>
+                        <h4>Utilisation</h4>
+                        <p class="item-util"><?= $langage["utilisation"] ?></p>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                &copy;Hy-Vong 2019
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ administration/liste-langages.php
@@ -0,0 +1,65 @@
+<?php
+    include("connexion.php");
+
+    $REQUETE_LISTE_LANGAGES = "SELECT id, nom, auteur, date, description, utilisation from langage;";
+
+    $set_utf=$connexion->prepare("SET NAMES UTF8"); 
+    $set_utf->execute(); 
+
+    $requete = $connexion->prepare($REQUETE_LISTE_LANGAGES);
+    $requete->execute();
+    $listeLangages = $requete->fetchAll();
+
+    //Affichage brut des données
+    //print_r($listeLangages);
+
+?>
+
+<html lang="fr" xml:lang="fr">
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+        <title>Liste des langages de programmation</title>
+        <link rel="stylesheet" type="text/css" href="css/liste.css">
+        <script>
+        function clicItem(idItem)
+        {
+            var url = "detail-langage.php?id="+ idItem;
+            console.log(url);
+            window.location = url;
+        }
+        </script>
+    </head>
+    <body>
+        <div id="contenu-page">
+            <header>
+                <h1>Liste des langages</h1>
+            </header>
+            <h2>Liste des langages de programmation</h2>
+            <div id="liste-item">
+                
+
+                <?php
+                    foreach($listeLangages as $langage)
+                    {
+                ?>
+
+                <div class="item-box" onclick="clicItem(<?= $langage['id'] ?>)">
+                    <!-- <a href="detail-langage.php?id="> -->
+                        <div class="item-img"></div>
+                    <!-- </a> -->
+                    <div class="item-text">
+                        <h3 class="item-name"><?= $langage["nom"] ?></h3>
+                        <p class="item-date"><?= $langage["date"] ?></p>
+                        <p class="item-desc"><?= $langage["description"] ?></p>
+                    </div>
+                </div>
+                <?php
+                    }
+                ?>
+            </div>
+            <footer>
+                &copy;Hy-Vong 2019
+            </footer>
+        </div>
+    </body>
+</html>
