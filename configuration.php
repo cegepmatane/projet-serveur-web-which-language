e1b301b42445ef723194edf70d8cd146cd621b4e
Hy-Vong Georges Dit Rap <hyvong@mail.freehv.me>
2021-02-28 18:23:06 -0500

    Version serveur opérationnelle

--- configuration.php
+++ configuration.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
-define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]. "/which-language/accesseurs/");
+define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]. "/accesseurs/");
 ?>
